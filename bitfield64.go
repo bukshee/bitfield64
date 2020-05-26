58bf3c6bea2e20ba709b20ed5712553c7cda2d0f
Adolf Szabo <adolf.szabo@gmail.com>
2020-05-26 17:37:36 +0000

    CHANGELOG added. New methods: Shift, Rotate, Left, Right, Mid

--- bitfield64.go
+++ bitfield64.go
@@ -26,12 +26,18 @@ func New() BitField64 {
 	return BitField64(0)
 }
 
+// the modulo treatment for positions outside of range [0,63]
+func posNormalize(pos int) int {
+	const n = 64
+	for pos < 0 {
+		pos += n
+	}
+	return pos % n
+}
+
 // pos => bit mask: 1 at bit position pos, 0 everywhere else
 func posToBitMask(pos int) BitField64 {
-	for pos < 0 {
-		pos += 64
-	}
-	return 1 << uint64(pos%64)
+	return 1 << uint64(posNormalize(pos))
 }
 
 // Set sets the bit at position pos
@@ -90,4 +96,53 @@ func (bf64 BitField64) Xor(bfo BitField64) BitField64 {
 // OnesCount returns the number of bits set
 func (bf64 BitField64) OnesCount() int {
 	return bits.OnesCount64(uint64(bf64))
+}
+
+// Mid returns count bits from position pos
+func (bf64 BitField64) Mid(pos, count int) BitField64 {
+	if count < 0 {
+		panic("count is negative")
+	}
+	const n = 64
+	count = count % n
+	pos = posNormalize(pos)
+	end := pos + count
+	a := bf64 << (n - end)
+	a = a >> (n - count)
+	return a
+}
+
+// Left returns leftmost count bits: [0, count-1]
+func (bf64 BitField64) Left(count int) BitField64 {
+	return bf64.Mid(0, count)
+}
+
+// Right returns rightmost count bits [63-count, 63]
+func (bf64 BitField64) Right(count int) BitField64 {
+	const n = 64
+	return bf64.Mid(n-count, count)
+}
+
+// Rotate rotates by count bits: Bits exiting at one end entering at the other end.
+// If count is positive it rotates towards higher positions; If negative it rotates
+// towards lower positions.
+func (bf64 BitField64) Rotate(count int) BitField64 {
+	if count == 0 {
+		return bf64
+	}
+	return BitField64(bits.RotateLeft64(uint64(bf64), count))
+}
+
+// Shift shift bits by count positions. Bits exiting at one end are discarded;
+// bits entering at the other end are zeroed.
+// If count is positive it shifts towards higher positions; If negative it shifts
+// towards lower positions.
+func (bf64 BitField64) Shift(count int) BitField64 {
+	if count == 0 {
+		return bf64
+	}
+	if count < 0 {
+		return bf64 >> -count
+	}
+	return bf64 << count
 }
--- bitfield64_test.go
+++ bitfield64_test.go
@@ -54,3 +54,65 @@ func Test1(t *testing.T) {
 		t.Error("should be 0")
 	}
 }
+
+func Test2(t *testing.T) {
+	a := New().Set(1).Set(3)
+	if a.Mid(0, 4).OnesCount() != 2 {
+		t.Error("should be 2")
+	}
+	if a.Mid(1, 1).OnesCount() != 1 {
+		t.Error("should be 1")
+	}
+	if a.Mid(3, 5).OnesCount() != 1 {
+		t.Error("should be 1")
+	}
+	if a.Mid(4, 5).OnesCount() != 0 {
+		t.Error("should be 0")
+	}
+	a = New().Set(0).Set(1).Set(4)
+	if a.Left(4).OnesCount() != 2 {
+		t.Error("should be 2")
+	}
+	if a.Left(1).OnesCount() != 1 {
+		t.Error("should be 1")
+	}
+	if !New().Set(63).Right(3).Get(2) {
+		t.Error("should be true")
+	}
+
+	if !a.Rotate(1).Get(5) {
+		t.Error("should be true")
+	}
+	if !a.Rotate(-1).Get(3) {
+		t.Error("should be true")
+	}
+	if !New().Set(5).Rotate(0).Rotate(1).Rotate(-1).Get(5) {
+		t.Error("should be true")
+	}
+
+	if !New().Set(0).Shift(1).Get(1) {
+		t.Error("should be true")
+	}
+	if !New().Set(1).Shift(-1).Get(0) {
+		t.Error("should be true")
+	}
+	if New().SetAll().Shift(63).OnesCount() != 1 {
+		t.Error("should be 1")
+	}
+	if New().SetAll().Shift(-65).OnesCount() != 0 {
+		t.Error("should be 0")
+	}
+	a = New().Set(0).Set(5).Shift(0).Shift(-1).Shift(1)
+	if !a.Get(5) {
+		t.Error("should be true")
+	}
+	if a.Get(0) {
+		t.Error("should be false")
+	}
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("The code did not panic")
+		}
+	}()
+	New().Mid(0, -1)
+}
